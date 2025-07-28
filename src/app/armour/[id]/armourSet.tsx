e8d57d703eca4cffa3f980d496978a81d8d9d667
SV3N77 <danny.jiang1234@hotmail.com>
2025-07-28 16:32:08 +1000

    add description of armor pieces

--- src/app/armour/[id]/armourSet.tsx
+++ src/app/armour/[id]/armourSet.tsx
@@ -16,7 +16,20 @@ export default async function ArmourSet({ id }: { id: string }) {
       <h1 className="text-4xl font-bold mb-5">{armourSet.name}</h1>
       <section className="flex flex-col gap-4">
         <div className="flex flex-row justify-between">
-          <div className="flex flex-col gap-2">Total Cost: {totalCost.totalZenny}</div>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-xl font-bold">Armor Pieces</h1>
+            <div className="flex flex-col gap-2">
+              {armourSet.pieces.map((piece) => (
+                <div key={piece.name} className="flex flex-row gap-2">
+                  <div className="flex flex-col">
+                    <div className="text-base font-bold">{piece.name}</div>
+                    <div className="text-sm">{piece.description}</div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="font-bold">Total Cost: {totalCost.totalZenny}</div>
+          </div>
           <div className="border border-gray-300 rounded-lg shadow-md p-4 flex flex-col gap-3">
             <Defense armour={armourSet} />
             <Resistances armour={armourSet} />
